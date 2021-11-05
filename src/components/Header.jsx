e10aff85bac1280567cf2a0ad981536529be6aff
Bryan Vera <bveracachay@gmail.com>
2021-11-05 14:30:14 -0500

    se agrego opcioones y cerrar session en sidebar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { ReactComponent as Hamburger } from '../asset/icon/dashboard.svg';
 import { ReactComponent as HamburgerX } from '../asset/icon/close.svg';
+import { ReactComponent as CerrarSesion } from '../asset/icon/cerrarSesion.svg';
 import foto from '../asset/img/foto-perfil.jpg';
 import { Link } from 'react-router-dom';
 
@@ -23,9 +24,9 @@ const Header = () => {
 			className={`bg-azulEntel text-white w-full py-4 px-4 relative flex items-center justify-between`}
 		>
 			<div
-				className={`bg-azulEntel h-screen z-20 ${propiedad} transform w-80 tras absolute left-0 top-0 pt-16  transition-all duration-700 `}
+				className={`bg-azulEntel h-screen z-20 ${propiedad} transform w-80 tras absolute left-0 top-0 pt-16  transition-all duration-500 flex flex-col justify-between`}
 			>
-				<div>
+				<div className="border-white border-t-2 ">
 					<HamburgerX
 						fill="white"
 						width="35px"
@@ -33,15 +34,16 @@ const Header = () => {
 						className={`absolute cursor-pointer z-30 top-20 left-4`}
 						onClick={() => probando(!menu)}
 					/>
-					<div className="py-4 px-4 flex w-full flex-col justify-center item-center bg-naranjaEntel ">
+					<div className="py-4 px-4 flex w-full flex-col justify-center font-barlow item-center bg-naranjaEntel border-b-4 border-white mb-8 ">
 						<img
 							src={foto}
 							alt="foto de perfil"
 							className="rounded-full w-32 mx-auto"
 						/>
-						<p className="text-center text-white my-4 text-2xl font-barlow font-bold">
+						<p className="text-center text-white mt-4 text-2xl font-barlow font-bold">
 							Pedro castillo
 						</p>
+						<span className="text-center mb-4">Administrador</span>
 					</div>
 					<div>
 						<Link
@@ -82,6 +84,19 @@ const Header = () => {
 						</Link>
 					</div>
 				</div>
+				<Link
+					to="/"
+					className="flex justify-start hover:bg-gray-50 items-center pb-2 pl-5"
+				>
+					<CerrarSesion
+						width="25px"
+						className="text-naranjaEntel"
+						height="25px"
+					/>
+					<button className="text-naranjaEntel font-barlow text-2xl w-40">
+						Cerrar Session
+					</button>
+				</Link>
 			</div>
 			<div className="flex justify-start items-center relative py-4  mr-10">
 				<Hamburger
