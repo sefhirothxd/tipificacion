665e03a165dff3c0796be0212894973667d948e0
Bryan Vera <bveracachay@gmail.com>
2021-11-25 22:40:55 -0500

    se arregla la url

--- src/components/admin/FormUsuarioNuevo.jsx
+++ src/components/admin/FormUsuarioNuevo.jsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
-import Select from 'react-select';
-import axios from 'axios';
 
 const FormUsuarioNuevo = ({ cerrarModal, agregarUsuario, listRol }) => {
 	const {
