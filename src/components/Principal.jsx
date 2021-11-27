7201a724b5ce0b4b2956ac672454adf18913e99a
Bryan Vera <bveracachay@gmail.com>
2021-11-27 13:47:01 -0500

    se agrega mas opciones en la pagina tipificacion

--- src/components/Principal.jsx
+++ src/components/Principal.jsx
@@ -52,71 +52,83 @@ const Principal = () => {
 		<div className="bg-gray-100 w-full content-container flex justify-center items-start">
 			<div className="flex justify-center items-center   w-full">
 				{estado ? (
-					<form className=" py-9 px-9 rounded-lg shadow-lg font-barlow flex justify-between flex-wrap items-center w-1200 mt-8 bg-white">
-						<div className="">
-							<h1>Informacion del cliente</h1>
-							<p>Nombre: {cliente && cliente[num]?.person.name}</p>
-							<p>Apellido: {cliente && cliente[num]?.person.lastname}</p>
-							<p>Rut: {cliente && cliente[num]?.person.numDoc}</p>
-							<p>Telefono: {cliente && cliente[num]?.person.telephone}</p>
-							<p>Correo: {cliente && cliente[num]?.person.email}</p>
+					<form className=" py-9 px-9 rounded-lg shadow-lg font-barlow flex flex-col justify-between flex-wrap  w-1200 mt-8 bg-white">
+						<h1 className="text-2xl md:text-4xl text-naranjaEntel text-center font-barlow font-semibold mb-8">
+							Tipificacion de llamadas
+						</h1>
+						<div className="flex justify-between w-full mb-5 items-center">
+							<div className="">
+								<h1>Informacion del cliente</h1>
+								<p>Nombre: {cliente && cliente[num]?.person.name}</p>
+								<p>Apellido: {cliente && cliente[num]?.person.lastname}</p>
+								<p>Rut: {cliente && cliente[num]?.person.numDoc}</p>
+								<p>Telefono: {cliente && cliente[num]?.person.telephone}</p>
+								<p>Correo: {cliente && cliente[num]?.person.email}</p>
+							</div>
+							<div className="flex flex-col mt-10 sm:mt-0">
+								<select name="select" className="w-60" id="">
+									{lista ? (
+										lista.map((item, index) => {
+											return (
+												item.nivel == 1 && (
+													<option key={index} value={item.titulo}>
+														{item.titulo}
+													</option>
+												)
+											);
+										})
+									) : (
+										<option value="0">No hay data</option>
+									)}
+								</select>
+								<select name="select" className="w-60 mt-3" id="">
+									{lista ? (
+										lista.map((item, index) => {
+											return (
+												item.nivel == 2 && (
+													<option key={index} value={item.titulo}>
+														{item.titulo}
+													</option>
+												)
+											);
+										})
+									) : (
+										<option value="0">No hay data</option>
+									)}
+								</select>
+								<select name="select" className="w-60 mt-3" id="">
+									{lista ? (
+										lista.map((item, index) => {
+											return (
+												item.nivel == 3 && (
+													<option key={index} value={item.titulo}>
+														{item.titulo}
+													</option>
+												)
+											);
+										})
+									) : (
+										<option value="0">No hay data</option>
+									)}
+								</select>
+							</div>
 						</div>
-						<div className="flex flex-col mt-10 sm:mt-0">
-							<select name="select" className="w-60" id="">
-								{lista ? (
-									lista.map((item, index) => {
-										return (
-											item.nivel == 1 && (
-												<option key={index} value={item.titulo}>
-													{item.titulo}
-												</option>
-											)
-										);
-									})
-								) : (
-									<option value="0">No hay data</option>
-								)}
-							</select>
-							<select name="select" className="w-60" id="">
-								{lista ? (
-									lista.map((item, index) => {
-										return (
-											item.nivel == 2 && (
-												<option key={index} value={item.titulo}>
-													{item.titulo}
-												</option>
-											)
-										);
-									})
-								) : (
-									<option value="0">No hay data</option>
-								)}
-							</select>
-							<select name="select" className="w-60" id="">
-								{lista ? (
-									lista.map((item, index) => {
-										return (
-											item.nivel == 3 && (
-												<option key={index} value={item.titulo}>
-													{item.titulo}
-												</option>
-											)
-										);
-									})
-								) : (
-									<option value="0">No hay data</option>
-								)}
-							</select>
-							<div>
-								<button
-									className="bg-naranjaEntel py-2 absolute top-500 px-4
+						<textarea
+							className="border-black border-2"
+							name=""
+							id=""
+							cols="30"
+							rows="10"
+						></textarea>
+						<div className="w-full flex justify-end">
+							<button
+								className="bg-naranjaEntel py-2  top-500 px-4
 								text-center text-2xl font-bold rounded text-white mb-4 md:my-8
 								font-barlow outline-none focus:outline-none"
-									onClick={(e) => siguienteCliente(e)}
-								>
-									Guardar
-								</button>
-							</div>
+								onClick={(e) => siguienteCliente(e)}
+							>
+								Guardar
+							</button>
 						</div>
 					</form>
 				) : (
